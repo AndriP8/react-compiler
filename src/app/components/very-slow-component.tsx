944ccc30186aba2f7281b43af8db935e79f424d6
Andri Purnomo <andri.adrp@gmail.com>
2024-06-18 11:18:39 +0700

    try react compiler

---
+++ src/app/components/very-slow-component.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React, { useEffect } from 'react';
+
+export const wait = (ms: number) => {
+  const start = Date.now();
+  let now = start;
+
+  while (now - start < ms) now = Date.now();
+};
+
+export const VerySlowComponent = (_: any) => {
+  wait(3000);
+  useEffect(() => {
+    console.log('re-render slow component');
+  });
+
+  return <div className="text-base my-4">Slow component</div>;
+};
